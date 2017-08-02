621a23480eb9df88a622820ec6881313ba16e6ab
Liam Globit <guoylyy@gmail.com>
2017-08-03 07:36:39 +0800

    1. 2017-08-03- update

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ def hello_world():
     print 'hello world'
     return render_template('test.html')
 
-@app.route('/A10223')
-def details():
-    return render_template('A10223.html')
+@app.route('/<string:student_number>/details')
+def details(student_number):
+    return render_template( str(student_number) + '.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
